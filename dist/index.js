aac03344d636165e9690fd8e1b140c333d95718b
Moses Martins <mmoses061@gmail.com>
2025-07-31 17:46:20 +0100

    Add Middlewares

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,40 @@
 import express from 'express';
+import { config } from './config.js';
 const app = express();
 const PORT = 8080;
+app.use(middlewareLogResponses);
+app.get("/reset", handlerReset);
+app.get("/metrics", handlerNumRequests);
 app.get("/healthz", handlerReadiness);
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.use("/app", express.static("./assets/logo.png"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
 function handlerReadiness(req, res) {
-    res.set('content-Type', 'text/plain');
+    res.set('Content-Type', 'text/plain');
     res.send("OK");
+}
+function handlerNumRequests(req, res) {
+    res.set('Content-Type', 'text/plain');
+    res.send(`Hits: ${config.fileserverHits}`);
+}
+function handlerReset(req, res) {
+    res.set('Content-Type', 'text/plain');
+    config.fileserverHits = 0;
+    res.send(`Reset!!}`);
+}
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        if (res.statusCode != 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
+function middlewareMetricsInc(req, res, next) {
+    res.on("finish", () => {
+        config.fileserverHits++;
+    });
+    next();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,49 @@
-import express, { Request, Response } from 'express';
-
+import express, { Request, Response, NextFunction } from 'express';
+import { config } from './config.js'
 import path from "path"
 
 const app = express();
 const PORT = 8080;
 
-app.get("/healthz", handlerReadiness);
+app.use(middlewareLogResponses)
 
-app.use("/app", express.static("./src/app"));
+app.get("/reset", handlerReset);
+app.get("/metrics", handlerNumRequests);
+app.get("/healthz", handlerReadiness);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.use("/app", express.static("./assets/logo.png"));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 })
 
-
-
 function handlerReadiness(req: Request, res: Response) {
-    res.set('content-Type', 'text/plain')
+    res.set('Content-Type', 'text/plain')
     res.send("OK")
+}
+
+function handlerNumRequests(req: Request, res: Response) {
+    res.set('Content-Type', 'text/plain')
+    res.send(`Hits: ${config.fileserverHits}`)
+}
+
+function handlerReset(req: Request, res: Response) {
+    res.set('Content-Type', 'text/plain')
+    config.fileserverHits = 0
+    res.send(`Reset!!`)
+}
+
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        if (res.statusCode != 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
+        }
+    })
+    next()
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+    config.fileserverHits++
+    next()
 }
